6c78b3690f518e5b17089b056fb2913771efc8a4
Dorian <dorian.fouvez@student.vinci.be>
2020-11-03 15:30:48 +0100

    Plateform Game Squeleton

---
+++ platformer/app.js
@@ -0,0 +1,48 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var session = require("express-session");
+
+// For /api request
+//var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+//app.set('views', path.join(__dirname, 'views'));
+//app.set('view engine', 'hbs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+// not found in static files, so default to index.html
+app.use((req, res, next) => {
+  if (!req.path.startsWith("/api/"))
+    return res.sendFile(`${__dirname}/public/index.html`);
+  next();
+});
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+// For /api request
+//app.use('/api/users', usersRouter);
+
+module.exports = app;
